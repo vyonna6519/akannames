9a94e4740e739876f5a8c97178c71d690d676b7c
vyonna <vyonna.njenga@student.moringaschool.com>
2021-12-12 20:31:08 +0300

    added get birthday function to form

--- index.js
+++ index.js
@@ -1,22 +1,62 @@
-function giveYear() {
-    var year = document.getElementById("yearBorn").value;
-    if (year == "") {
-      alert("Year must be filled ");
-      return false;
+function getBirthday() {
+    
+    let gender = document.getElementById("gender").value;
+    let dob = document.getElementById("start").value;
+
+    let dateStr = new Date(dob);
+    let dayofWeek = dateStr.getDay();
+
+     //console.log(dayofWeek);
+     //console.log(gender);
+
+
+     getAkanName(dayofWeek, gender);
+
+};
+
+function getAkanName(dayofWeek, gender) {
+    let day = dayofWeek;
+    let week = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ]
+    let male = [
+         "Kwasi",
+         "Kwadwo",
+         "Kwabena",
+         "Kwaku",
+         "Yaw",
+         "Kofi",
+         "Kwame"];
+
+    let female = [ 
+        "Akosua",
+        "Adwoa",
+        "Abenaa",
+        "Akua",
+        "Afua",
+        "Ama"];
+
+
+    if (gender === "Male") {
+        let akanPair = male[day];
+
+        let weekDay = week[day];
+
+        document.getElementById("Akandisplay").innerHTML = " Your Akan name is " + akanPair + " since you were born on a " + weekDay;
+
+
+    } else if (gender === "Female") {
+        let akanPair = female[day];
+        document.getElementById("Akandisplay").innerHTML = " Your Akan name is " + akanPair + " since you were born on a " + weekDay;
+    } else {
+
+        console.log("The Gender or Date can not be empty")
     }
-  }
-  
-  function giveMonth() {
-    var month = document.getElementById("monthBorn").value;
-    if (month == "0") {
-      alert("Month must be filled");
-      return false;
-    }
-  }
-  
-  function giveDay() {
-    var day = document.getElementById("dayBorn").value;
-    if (day == "") {
-      alert("Day must be filled");
-      return false;
-    }
+
+}
