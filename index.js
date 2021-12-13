4aee63e0ef1dea33de51a0f319c0d6f5d39885c0
vyonna <vyonna.njenga@student.moringaschool.com>
2021-12-13 21:16:36 +0300

    added var to getAkanForm function

--- index.js
+++ index.js
@@ -1,58 +1,41 @@
 function getBirthday() {
     
-    let gender = document.getElementById("gender").value;
-    let dob = document.getElementById("start").value;
-
-    let dateStr = new Date(dob);
-    let dayofWeek = dateStr.getDay();
-
-     getAkanName(dayofWeek, gender);
-
-};
-
-function getAkanName(dayofWeek, gender) {
-    let day = dayofWeek;
-    let week = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ]
-    let male = [
-         "Kwasi",
-         "Kwadwo",
-         "Kwabena",
-         "Kwaku",
-         "Yaw",
-         "Kofi",
-         "Kwame"];
-
-    let female = [ 
-        "Akosua",
-        "Adwoa",
-        "Abenaa",
-        "Akua",
-        "Afua",
-        "Ama"];
-
-
+    var gender = document.getElementById("gender").value;
+    var dob = document.getElementById("start").value;
+  
+    var dateStr = new Date(dob);
+    var dayofWeek = dateStr.getDay();
+  
+     //console.log(dayofWeek);
+     //console.log(gender);
+  
+  
+      getAkanForm(dayofWeek, gender);
+  
+  };
+  
+    var week = [ "Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday",]
+    var male = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+   var female = ["Akosua","Adwoa","Abenaa","Akua","Afua","Ama"];
+  
+  function getAkanForm(dayofWeek, gender) {
+    
+    var day = dayofWeek;
+  
     if (gender === "Male") {
-        let akanPair = male[day];
-
-        let weekDay = week[day];
-
-        document.getElementById("Akandisplay").innerHTML = " Your Akan name is " + akanPair + " since you were born on a " + weekDay;
-
-
+        var name = male[day];
+  
+        var weekDay = week[day];
+  
+        document.getElementById("Akanprint").innerHTML = " You Were Born on "  + "" + weekDay + " " + "Your Akan Name Is" + name;
+  
+  
     } else if (gender === "Female") {
-        let akanPair = female[day];
-        document.getElementById("Akandisplay").innerHTML = " Your Akan name is " + akanPair + " since you were born on a " + weekDay;
+        var name = female[day];
+        document.getElementById("Akanprint").innerHTML = " You Were Born on "  + "" + weekDay + " " + "Your Akan Name Is" + name;
     } else {
-
+  
         console.log("The Gender or Date can not be empty")
     }
-
-}
+  
+  }
